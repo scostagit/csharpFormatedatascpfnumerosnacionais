12f6d0f48b175622fe9d799c636022de753078b2
SLAINTE\scosta <scosta@vitrosotware.com>
2018-04-13 16:35:23 +0100

    inicio do projecto

---
+++ templete.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Collections
+{
+
+
+    class Program
+    {
+      
+      static void Main(string[] args)
+      {
+     
+        
+      }
+      
+     
+    }
+
+
+}
